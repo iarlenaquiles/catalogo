77cda5c7a5af1def609ded940f1a34ef8e101247
Iarlen <iarlensilva775@gmail.com>
2017-12-20 23:25:37 -0300

    Usando o Axios

--- src/components/ListaItens.js
+++ src/components/ListaItens.js
@@ -6,12 +6,18 @@ import axios from 'axios';
 import Itens from './Itens';
 
 export default class ListaItens extends Component {
+	componentWillMount() {
+		axios.get('http://faus.com.br/recursos/c/dmairr/api/itens.html')
+			.then(response => { console.log(response); })
+			.catch(() => { console.log('Erro ao recuperar os dados'); });
+	}
+
 	render() {
 		return (
 			<View>
-			<Itens />
-			<Itens />
-			<Itens />
+				<Itens />
+				<Itens />
+				<Itens />
 			</View>
 			);
 		}
