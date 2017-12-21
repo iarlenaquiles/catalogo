769297b507b6b430ee5a16733ea9c4f9c27927b0
Iarlen <iarlensilva775@gmail.com>
2017-12-20 21:57:32 -0300

    Limpando as funções de ciclo de vida

--- index.android.js
+++ index.android.js
@@ -7,21 +7,7 @@ import ListaItens from './src/components/ListaItens';
 
 class catalogo extends Component {
 
-  constructor(props) {
-    super(props);
-    console.log('Construindo a aplicação');
-  }
-
-  componentWillMount() {
-    console.log('Fazer alguma coisa antes de renderizar');
-  }
-
-  componentDidMount() {
-    console.log('Fazer alguma coisa depois de renderizar');
-  }
-
   render() {
-    console.log('Objeto é renderizado');
     return (
      <ListaItens />
     );
