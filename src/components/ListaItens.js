cfa291bf3183546da5136a3ed02c7cde9b54f69a
Iarlen <iarlensilva775@gmail.com>
2017-12-23 13:35:48 -0300

    Mudando o plano de fundo

--- src/components/ListaItens.js
+++ src/components/ListaItens.js
@@ -20,7 +20,7 @@ export default class ListaItens extends Component {
 
 	render() {
 		return (
-			<ScrollView>
+			<ScrollView style={{backgroundColor: '#DDD'}}>
 			{ this.state.listaItens.map(item => <Itens key={item.titulo} item={item} />) }
 			</ScrollView>
 			);
